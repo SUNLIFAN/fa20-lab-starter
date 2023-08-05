d9a037c85b16dc7a7d3cb8728440a0ade585aabd
SUNLIFAN <lifansun1412@gmail.com>
2023-08-06 00:06:18 +0800

    lab01 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+    node* slow = head;
+    node* fast = head;
+
+    while(fast != NULL) {
+        fast = fast->next;
+        if (fast == NULL) {
+            return 0;
+        }
+        fast = fast->next;
+        slow = slow->next;
+        if (slow == fast) {
+            return 1;
+        }
+    }
+
     return 0;
 }
